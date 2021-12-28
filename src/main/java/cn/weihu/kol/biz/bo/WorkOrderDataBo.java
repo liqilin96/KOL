b39819dde9fccc7df49f1ef3bcbfdd3b847dc9e3
lql <liqilin@95114.cn>
2021-12-28 14:54:22 +0800

    修改参数访问修饰符

--- src/main/java/cn/weihu/kol/biz/bo/WorkOrderDataBo.java
+++ src/main/java/cn/weihu/kol/biz/bo/WorkOrderDataBo.java
@@ -9,135 +9,135 @@ import lombok.Data;
 public class WorkOrderDataBo {
 
         // 序号
-        String index;
+        private String index;
 
         // 媒体
-        String media;
+        private String media;
 
         // 账号
-        String account;
+        private String account;
 
         // 账号ID或链接
-        String IDorLink;
+        private String IDorLink;
 
         // 账号类型
-        String accountType;
+        private String accountType;
 
         // 资源位置
-        String address;
+        private String address;
 
         // 数量
-        String count;
+        private String count;
 
         // 发布开始时间
-        String postStartTime;
+        private String postStartTime;
 
         // 发布结束时间
-        String postEndTime;
+        private String postEndTime;
 
         // 电商链接
-        String linkPrice;
+        private String linkPrice;
 
         // @
-        String at;
+        private String at;
 
         // 话题
-        String topic;
+        private String topic;
 
         // 电商肖像授权
-        String storeAuth;
+        private String storeAuth;
 
         // 品牌双微转发授权
-        String shareAuth;
+        private String shareAuth;
 
         // 微任务
-        String microTask;
+        private String microTask;
 
         // 报备
-        String report;
+        private String report;
 
         // 信息流授权
-        String msgAuth;
+        private String msgAuth;
 
         // 线下探店
-        String face;
+        private String face;
 
         // 星图/ 快接单
-        String star;
+        private String star;
 
         // 其他
-        String other;
+        private String other;
 
         // 产品提供方
-        String product;
+        private String product;
 
         // 发布内容brief概述
-        String brief;
+        private String brief;
 
         // 星图/ 快接单平台截图
-        String img;
+        private String img;
 
         // 截图时间
-        String imgTime;
+        private String imgTime;
 
         // 星图/ 快接单平台报价
-        String platPrice;
+        private String platPrice;
 
         // 粉丝数
-        String fansCount;
+        private String fansCount;
 
         // 报价
-        String price;
+        private String price;
 
         // 佣金
-        String commission;
+        private String commission;
 
         // 备注
-        String remark;
+        private String remark;
 
         // 供应商
-        String supplier;
+        private String supplier;
 
         // 档期开始时间
-        String scheduleStartTime;
+        private String scheduleStartTime;
 
         // 档期结束时间
-        String scheduleEndTime;
+        private String scheduleEndTime;
 
         // 历史报价
-        String history;
+        private String history;
 
         // 反馈
-        String feedback;
+        private String feedback;
 
         // 保价到期时间
-        String priceTime;
+        private String priceTime;
 
         // 入库时间
-        String inTime;
+        private String inTime;
 
         // 电商授权
-        String retailAuth;
+        private String retailAuth;
 
         // 肖像授权
-        String portAuth;
+        private String portAuth;
 
         // 折扣
-        String sale;
+        private String sale;
 
         // 仅保价一天
-        String priceOnlyDay;
+        private String priceOnlyDay;
 
         /**
          * 逻辑字段
          */
 
         // 是否 库内 "0" 库内  "1" 库外
-        String inbound;
+        private String inbound;
 
         // 达人唯一标识
-        String actorSn;
+        private String actorSn;
 
         // 二选一分组使用标识
-        String compareFlag;
+        private String compareFlag;
 }
